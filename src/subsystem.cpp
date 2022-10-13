ce8974bea15ec6970aaae8f5caa0fffc8155874a
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2022-10-13 01:52:18 +0000

    Add 3 shoot modes with modkey

--- src/subsystem.cpp
+++ src/subsystem.cpp
@@ -1,5 +1,35 @@
 #include "main.h"
+bool L2Pressed = false;
+bool L1Pressed = false;
 
 void index() {
-    indexer.moveRelative(360, 600);
-}
+    //Check if modkey is active for long shoot
+    if (modKey) {
+         if (controller.getDigital(ControllerDigital::L2) && !L2Pressed) {
+            indexer.moveRelative(360, 200);
+            L2Pressed = true;
+        }
+        else {
+            L2Pressed = false;
+        }
+    }
+    //Else run rapid shoot 
+    else {
+        //Rapid fire 3 discs
+        if (controller.getDigital(ControllerDigital::L2) && !L2Pressed) {
+            indexer.moveRelative(360 * 3, 600);
+            L2Pressed = true;
+        }
+        else {
+            L2Pressed = false;
+        }
+        //Shoot single disc
+        if (controller.getDigital(ControllerDigital::L1) && !L1Pressed) {
+            indexer.moveRelative(360, 600);
+            L1Pressed = true;
+        }
+        else {
+            L1Pressed = false;
+        }
+    }
+} 
